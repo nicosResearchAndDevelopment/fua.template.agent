4209fe4391a6f93aa8e6bc50ea7e2ccb3eefbb54
Simon Petrac <pfoerdie@gmail.com>
2023-10-10 14:32:43 +0200

    wip

--- src/agent.js
+++ src/agent.js
@@ -3,10 +3,10 @@ const
     {name: identifier} = require('../package.json'),
     assert             = require('@nrd/fua.core.assert');
 
-// TODO implement agent functionality
-
 assert(!global[identifier], 'unable to load a second uncached version of the singleton ' + identifier);
 Object.defineProperty(global, identifier, {value: agent, configurable: false, writable: false, enumerable: false});
+
+// TODO implement agent functionality
 
 Object.freeze(agent);
 module.exports = agent;
